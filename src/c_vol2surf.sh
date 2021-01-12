06cf42a77e2005a97d66da44b68869a2903ef8ea
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2021-01-12 10:48:46 -0600

    Edit script for projection within PNC template space

--- src/c_vol2surf.sh
+++ src/c_vol2surf.sh
@@ -8,14 +8,14 @@ source $FREESURFER_HOME/SetUpFreeSurfer.sh
 export SUBJECTS_DIR=$FREESURFER_HOME/subjects # edit path
 #recon-all -i data/template/pnc_template.nii.gz -s pnc_template -all
 
-# Do I even need this? 2. register brain to pnc_template
+# 3. register brain to pnc_template
 export SUBJECTS_DIR=$FREESURFER_HOME/subjects # edit path
 export PNC_BRAIN=data/template/pnc_template_brain.nii.gz # edit path
 export TRANSFORM=data/transform/identity.dat # edit path
 
 fslregister --s pnc_template --mov $PNC_BRAIN --reg $TRANSFORM
 
-# 3. project to surface
+# 4. project to surface
 export CBF_IMG=/Users/vgonzenb/PennSIVE/ISLA/data/mri/avgCBF_thr10.nii.gz # edit path 
 export CBF_SURF=/Users/vgonzenb/PennSIVE/ISLA/data/surf/c_lh.${CBF_IMG:39:12}.mgz # edit ouput path (does not exist at runtime)
 
@@ -27,4 +27,4 @@ mri_vol2surf --mov $CBF_IMG \
 
 # 4. visualize surface
 freeview  -f $SUBJECTS_DIR/pnc_template/surf/lh.inflated::overlay=$CBF_SURF:overlay_method=piecewise:overlay_threshold=20,55,90 \
---viewport 3d
+--viewport 3d 
