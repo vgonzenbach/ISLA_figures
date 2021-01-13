d55f6c8ef57ae2eee3072b3d8074827be5908a7c
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2021-01-13 10:40:22 -0600

    Edit surface projection script

--- src/z_vol2surf.sh
+++ src/z_vol2surf.sh
@@ -1,3 +1,17 @@
+#!/usr/bin/bash
+echo """
+Projecting CBF img onto surface after ANTs transformation
+
+Volume Transformation:
+    Module: ANTs
+    Mov: Avg. CBF (in PNC 2mm space); Threshold=10%; 
+    Target: MNI (2mm) 
+
+Surface Projection:
+    Module: Freesurfer
+    Mov: Avg. CBF (in MNI 2mm space)
+    Target: fsaverage
+"""
 ## 1. Init FreeSurfer
 export FREESURFER_HOME=/Applications/freesurfer/7.1.1
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
@@ -10,14 +24,15 @@ export CBF_MNI=$PWD/data/mri/reg/avgCBF_thr10_MNI.nii.gz
 
 antsApplyTransforms -i $CBF_PNC -o $PWD/data/mri/reg/avgCBF_thr10_MNI.nii.gz -t $TRANSFORM_DIR/PNC-MNI_0warp.nii.gz -t $TRANSFORM_DIR/PNC-MNI_1Affine.mat \
     -r $PWD/data/templates/MNI-2x2x2.nii.gz -d 3 -e 3 -n LanczosWindowedSinc
-echo "Transform complete"
+echo "Transform complete 
+"
 
 ## 3. Project
-export CBF_SURF=$PWD/data/surf/z_lh.avgCBF_thr10.nii.gz
-mri_vol2surf --mov $CBF_MNI  --regheader fsaverage5 --hemi lh \
-    --o $CBF_SURF --projfrac-avg 0 1 0.1 --surf white
+export CBF_SURF=$PWD/tmp/z_lh.avgCBF_thr10.mgz
+mri_vol2surf --mov $CBF_MNI --reg $FREESURFER_HOME/average/mni152.register.dat --hemi lh \
+    --projfrac-avg 0 1 0.1 --surf white --surf-fwhm 3 --o $CBF_SURF
 echo "Projection complete"
 
 ## 4. Visualize
-freeview  -f $SUBJECTS_DIR/fsaverage5/surf/lh.inflated::overlay=$CBF_SURF:overlay_method=piecewise:overlay_threshold=20,55,90 \
+freeview  -f $SUBJECTS_DIR/fsaverage/surf/lh.inflated::overlay=$CBF_SURF:overlay_method=piecewise:overlay_threshold=20,55,90 \
 --viewport 3d
