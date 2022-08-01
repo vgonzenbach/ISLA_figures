b9b9ebe3fdbe154563a2c71ebd33fa45409f3c2a
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2022-08-01 14:55:05 -0400

    Delete old, unused files and scripts

--- src/fslregister_pnc_brain.sh
+++
@@ -1,7 +0,0 @@
-export FREESURFER_HOME=/Applications/freesurfer/7.1.1
-source $FREESURFER_HOME/SetUpFreeSurfer.sh
-
-export PNC_BRAIN=data/pncTemplate/pnc_template_brain.nii.gz
-
-fslregister --s fsaverage --mov $PNC_BRAIN --reg brain.dat
-#tkregister2 --targ $PNC_BRAIN --mov $PNC_BRAIN --s identity -reg tmp.dat
